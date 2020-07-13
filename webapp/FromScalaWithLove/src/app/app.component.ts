c46e673430c637843db0821aa3402716dd50a192
antoine <antoine.doeraene@b12-consulting.com>
2020-07-13 22:18:54 +0200

    first use of the scala service

--- webapp/FromScalaWithLove/src/app/app.component.ts
+++ webapp/FromScalaWithLove/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ArrayEnhanced } from "scala-module";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'FromScalaWithLove';
+  title = "FromScalaWithLove";
+
+  constructor(arrayEnhanced: ArrayEnhanced) {
+    const x = [1, 2, 3, 1, 2];
+
+    console.log(arrayEnhanced.distinct(x));
+    console.log(arrayEnhanced.distinctBy(x, (e) => e % 2));
+  }
 }
--- webapp/FromScalaWithLove/src/app/app.module.ts
+++ webapp/FromScalaWithLove/src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ArrayEnhanced } from "scala-module";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [ArrayEnhanced],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

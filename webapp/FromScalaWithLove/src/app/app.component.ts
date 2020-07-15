210c64d129a55e9f955580ba3f7bf848848e4870
antoine <antoine.doeraene@b12-consulting.com>
2020-07-15 22:54:17 +0200

    zio service usage via console

--- webapp/FromScalaWithLove/src/app/app.component.ts
+++ webapp/FromScalaWithLove/src/app/app.component.ts
@@ -11,5 +11,31 @@ import { delay, tap } from "rxjs/operators";
 export class AppComponent {
   title = "FromScalaWithLove";
 
-  constructor(zioService: ZIOService) {}
+  constructor(zioService: ZIOService) {
+    const program = (j: number) =>
+      of(j).pipe(
+        delay(1000),
+        tap((j) => {
+          if (Math.random() > 0.9) {
+            console.warn(`boom ${j}`);
+            throw new Error(`${j} kaboom`);
+          }
+        }),
+        tap((e) => console.log(e))
+      );
+
+    const completionState = zioService.foreachParN(
+      [...new Array(30).keys()],
+      program,
+      2,
+      3
+    );
+
+    completionState.progress.forEach((p) => console.log("progress", p));
+    completionState.result.forEach((r) => console.log("result", r));
+
+    // setTimeout(() => {
+    //   completionState.cancel();
+    // }, 1500);
+  }
 }

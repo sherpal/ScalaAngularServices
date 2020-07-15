052ec6e7fe235ab0d679f873910ac130da5b45d3
antoine <antoine.doeraene@b12-consulting.com>
2020-07-15 22:52:58 +0200

    cleaning app component before zio service

--- webapp/FromScalaWithLove/src/app/app.component.ts
+++ webapp/FromScalaWithLove/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from "@angular/core";
-import {
-  ArrayEnhanced,
-  EmitRxObservable,
-  User,
-  UserService,
-} from "scala-module";
+import { ZIOService } from "scala-module";
+import { of } from "rxjs";
+import { delay, tap } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
@@ -14,35 +11,5 @@ import {
 export class AppComponent {
   title = "FromScalaWithLove";
 
-  constructor(
-    arrayEnhanced: ArrayEnhanced,
-    emitRx: EmitRxObservable,
-    userService: UserService
-  ) {
-    const x = [1, 2, 3, 1, 2];
-
-    console.log(arrayEnhanced.distinct(x));
-    console.log(arrayEnhanced.distinctBy(x, (e) => e % 2));
-
-    emitRx.naturalNumbers.forEach((e) => console.log(e));
-
-    console.log(
-      userService.usersByDateOfBirth([
-        new User(
-          "id1",
-          "Alice",
-          new Date("1990-01-05"),
-          new Date("2020-07-15")
-        ),
-        new User("id2", "Bob", null, new Date("2020-07-14")),
-        new User(
-          "id3",
-          "Charlie",
-          new Date("1990-07-04"),
-          new Date("2020-06-14")
-        ),
-        new User("id4", "Dean", new Date("1991-09-18"), new Date("2020-07-16")),
-      ])
-    );
-  }
+  constructor(zioService: ZIOService) {}
 }
--- webapp/FromScalaWithLove/src/app/app.module.ts
+++ webapp/FromScalaWithLove/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { ArrayEnhanced, EmitRxObservable, UserService } from "scala-module";
+import {
+  ArrayEnhanced,
+  EmitRxObservable,
+  UserService,
+  ZIOService,
+} from "scala-module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [ArrayEnhanced, EmitRxObservable, UserService],
+  providers: [ArrayEnhanced, EmitRxObservable, UserService, ZIOService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
